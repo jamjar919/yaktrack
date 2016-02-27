f633ac02e900a3c2fad33790aea1bbe4d340d0da
James Paterson <james@mjsquare.com>
2016-02-27 16:51:13 +0000

    Get all yaks - finally working...

--- yaktrack.py
+++ yaktrack.py
@@ -8,5 +8,20 @@ response = "";
 for line in p.stdout.readlines():
     response = response + str(line);
 retval = p.wait();
-
-print(response);
+response = str(response);
+response = response[3:len(response)-2];
+# Fix escaped characters that otherwise break the json interpreter
+response = response.replace("\\\\\"",""); # \\"
+response = response.replace("\\\'",""); # \'
+response = response.replace("\\",""); # \
+jsonData = response.split("},{"); # Response is not properly formatted for some reason, seperate and parse individually.
+yaks = list();
+for data in jsonData:
+    if data[0] != "{":
+        data = "{" + data;
+    if data[-1] != "}":
+        data = data + "}";
+    data = json.loads(data);
+    yaks.append(data["message"]);
+    
+print(yaks);
