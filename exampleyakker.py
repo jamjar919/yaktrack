e88ffd818cc12746755989f9d759928f4bb27f3d
James Paterson <james@mjsquare.com>
2016-03-02 11:26:49 +0000

    Removed some parameters in exampleyakker, as they were not needed

--- exampleyakker.py
+++ exampleyakker.py
@@ -1,26 +1,11 @@
 def getDefaultYid(): #yid changes every time the page updates, paste the first yid here
     return "";
 
-def getEndUserID(): # optimizelyEndUserId in cookie
-    return "";
-
-def get_ga(): # in cookie
-    return "";
-
-def getAmplitudeID(): #in cookie, does not change (?)
-    return "";
-
 def get__cfduid(): # in cookie
-    return "";
-
-def getOptimiserSegments(): # in cookie
-    return "";
-
-def getOptimiserBuckets(): # in cookie
     return "";
 
 def getUserAgent(): # User agent you want to emulate
     return "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36";
 
 def getCookie(yid): # leave this as it is
-    return "__cfduid="+get__cfduid()+"; optimizelyEndUserId="+getEndUserID()+"; optimizelySegments="+getOptimiserSegments()+"; optimizelyBuckets="+getOptimiserBuckets()+"; _ga="+get_ga()+"; amplitude_idyikyak.com="+getAmplitudeID()+"; yid="+yid+"; rm=true";
+    return "__cfduid="+get__cfduid()+"; yid="+yid+"; rm=true";
